c0dcb0507a1b678172638836297f64a987ed25d5
Jessica Dembe <jmdembe@gmail.com>
2016-10-31 16:06:39 -0400

    Added code to move between previous and next images

--- js/ui.js
+++ js/ui.js
@@ -12,7 +12,21 @@
 
       $('nav').on('click', 'button', function nextImage(event) {
           event.stopPropagation();
-          console.log("HELLO.");
+          var $target = $(event.target);
+          var previousImage = $(this).closest('li').prev();
+          var nextImage = $(this).closest('li').next();
+
+          if ($target.is('.previous')) {
+              previousImage.toggleClass('zoomed');
+          }
+
+          if ($target.is ('.next')) {
+              nextImage.toggleClass('zoomed');
+          }
+
+
+          // once you have the li... you can call .next() or .prev()
+          // then what do you need to add to that new li?
 
       })
 
