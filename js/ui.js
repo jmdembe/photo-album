4705de0917f2ee29b74a4fc3203f83dec721f7a2
Jessica Dembe <jmdembe@gmail.com>
2016-10-31 15:17:25 -0400

    Added script tag for UI and added zoom functionality for images

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,19 @@
 (function (){
     'use strict';
 
+    $('main')
+        .find('.photos')
+            .on('click', 'li', function zoomImage (event) {
+                $('main').toggleClass('zooming')
+                $(this)
+                  .toggleClass('zoomed')
+                  .append($('nav'));
 
+            })
 
-
+      $('nav').on('click', 'button', function nextImage(event) {
+          var target = $(event.target);
+          console.log("is this happening?");
+      })
 
 })();
